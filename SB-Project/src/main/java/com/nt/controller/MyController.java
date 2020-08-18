d25babeef1e376559b40d3bed2d690fdea9b9732
Atif-Nayar-Ansari <atifnayaransari999@gmail.com>
2020-08-18 23:07:21 +0530

    Everything is removed

--- SB-Project/src/main/java/com/nt/controller/MyController.java
+++ SB-Project/src/main/java/com/nt/controller/MyController.java
@@ -7,13 +7,7 @@ public class MyController {
 	
 	public void fun1() {
 		
-		int i = 10;
-		String s = "atif ansari";
-		char ch = 'f';
-		char ch2 = 'k';
-		char ch3 = 'o';
-		char ch4 = 'h';
-		char ch5 = 'p';
+		
 	}
 
 }
