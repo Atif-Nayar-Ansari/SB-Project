167b716ae14705f9f51e5cd8f293a53c009cbdc9
Atif-Nayar-Ansari <atifnayaransari999@gmail.com>
2020-08-18 15:15:23 +0530

    SB-02-New MyController class added with int i = 10

---
+++ SB-Project/src/main/java/com/nt/controller/MyController.java
@@ -0,0 +1,13 @@
+package com.nt.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+	
+	public void fun1() {
+		
+		int i = 10; 
+	}
+
+}
